05841b27beb6c296a7cb5dc27d7cca288bbd8bff
ilyastritskiy <msv_ilya@mail.ru>
2025-01-12 23:53:39 -0800

    feat(API): creating API endpoints for obtaining and editing a user profile and resetting a user account password in accordance with the terms of reference

--- PulseBackend/users/views.py
+++ PulseBackend/users/views.py
@@ -1,13 +1,16 @@
+from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework import status
-from rest_framework.generics import CreateAPIView
+from rest_framework.generics import CreateAPIView, RetrieveUpdateAPIView, UpdateAPIView
 from .models import CustomUser
-from .serializers import UserRegistrationSerializer
+from .serializers import UserRegistrationSerializer, UserProfileSerializer, UserUpdatePasswordSerializer
 
 
 class PingAPIView(APIView):
-    """Checks if the server is ready to handle requests."""
+    """
+    Checks if the server is ready to handle requests.
+    """
     def get(self, request):
         return Response("ok", status=status.HTTP_200_OK)
 
@@ -18,3 +21,42 @@ class UserRegisterAPIView(CreateAPIView):
     """
     queryset = CustomUser.objects.all()
     serializer_class = UserRegistrationSerializer
+
+
+class UserProfileAPIView(RetrieveUpdateAPIView):
+    """
+    Allows the authenticated user to retrieve or update their own profile.
+    """
+    serializer_class = UserProfileSerializer
+    permission_classes = [IsAuthenticated]
+
+    def get_object(self):
+        """
+        Override to return the currently authenticated user.
+        """
+        return self.request.user
+
+
+class UserUpdatePasswordAPIView(UpdateAPIView):
+    """
+    Handles password updates for the authenticated user.
+    """
+    serializer_class = UserUpdatePasswordSerializer
+    permission_classes = [IsAuthenticated]
+
+    def get_object(self):
+        """
+        Override to return the currently authenticated user.
+        """
+        return self.request.user
+
+    def partial_update(self, request, *args, **kwargs):
+        """
+        Override partial_update to modify the success response.
+        """
+        response = super().partial_update(request, *args, **kwargs)
+
+        if response.status_code == 200:
+            return Response({"status": "ok"}, status=response.status_code)
+
+        return response
