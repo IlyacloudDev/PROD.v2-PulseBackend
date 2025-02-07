5baba3d99c13489a3a872cfa7f59be9ba36184d2
ilyastritskiy <msv_ilya@mail.ru>
2025-02-07 14:51:41 +0300

    doc(doc-strings): added a missing doc string for the method of the custom permission class

--- PulseBackend/posts/permissions.py
+++ PulseBackend/posts/permissions.py
@@ -6,6 +6,10 @@ class IsPostAccessiblePermission:
         return True
 
     def has_object_permission(self, request, view, obj):
+        """
+        Provides access to the post if the author of the post has a public
+        profile or the user is a friend of the author of the post.
+        """
         if request.user == obj.author:
             return True
 
